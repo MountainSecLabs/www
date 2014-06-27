3071157240c130985d772eccff7a956109e4f32c
Tim <tfowler@sabaitechnology.com>
2014-06-27 10:56:09 -0400

    update

--- js/templatemo_script.js
+++ js/templatemo_script.js
@@ -1,9 +1,9 @@
-var top_menu_height = 0;
+ top_menu_height = 0;
 jQuery(function($) {
         $(document).ready( function() {
 
             // load google map
-        var script = document.createElement('script');
+ script = document.createElement('script');
         script.type = 'text/javascript';
         script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' +
             'callback=initialize';
@@ -24,7 +24,7 @@ jQuery(function($) {
         // scroll to specific id when click on menu
         $('.templatemo-top-menu .navbar-nav a').click(function(e){
             e.preventDefault(); 
-            var linkId = $(this).attr('href');
+     linkId = $(this).attr('href');
             scrollTo(linkId);
             if($('.navbar-toggle').is(":visible") == true){
                 $('.navbar-collapse').collapse('toggle');
@@ -41,7 +41,7 @@ jQuery(function($) {
             e.preventDefault(); 
             $(this).parent().children('a').removeClass('active');
             $(this).addClass('active');
-            var linkClass = $(this).attr('href');
+     linkClass = $(this).attr('href');
             $('.gallery').each(function(){
                 if($(this).is(":visible") == true){
                    $(this).hide();
@@ -60,21 +60,18 @@ jQuery(function($) {
     });
 });
 
-function initialize() {
-    var mapOptions = {
+function initialize() { mapOptions = {
       zoom: 12,
       center: new google.maps.LatLng(35.576638,-82.570899)
     };
-
-    var map = new google.maps.Map(document.getElementById('map-canvas'),  mapOptions);
+ map = new google.maps.Map(document.getElementById('map-canvas'),  mapOptions);
 }
 
 // scroll animation 
 function scrollTo(selectors)
 {
 
-    if(!$(selectors).size()) return;
-    var selector_top = $(selectors).offset().top - top_menu_height;
+    if(!$(selectors).size()) return; selector_top = $(selectors).offset().top - top_menu_height;
     $('html,body').animate({ scrollTop: selector_top }, 'slow');
 
 }
